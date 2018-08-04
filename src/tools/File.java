06de420822c213cf3be75b79276c07293bc2f6a8
TrimBallanca <trim.ballanca@gmail.com>
2018-08-04 11:20:44 -0400

    initial classes

---
+++ src/tools/File.java
@@ -0,0 +1,25 @@
+package tools;
+
+public class File {
+	private String name;
+	private String path;
+	private String delimiter;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getDelimiter() {
+		return delimiter;
+	}
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
+	}
+}
